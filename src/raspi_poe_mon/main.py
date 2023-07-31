c96c828d91101a5411937d6dce80371889beefa6
Sebastian Straub <sstraub@posteo.de>
2023-07-31 14:28:02 +0200

    add custom 5px font and write with it

--- src/raspi_poe_mon/main.py
+++ src/raspi_poe_mon/main.py
@@ -1,9 +1,14 @@
 import logging
+import pkgutil
 import time
+from io import BytesIO
 
 import typer
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
+from PIL import ImageFont
+from PIL.ImageDraw import ImageDraw
+from PIL.ImageFont import FreeTypeFont
 
 from raspi_poe_mon import __title__, __version__
 from raspi_poe_mon.poe_hat import PoeHat
@@ -42,17 +47,27 @@ def main(config_file: str = ConfigOption, version: bool = VersionOption):
     logger.info("Raspi PoE HAT Demo")
 
     poe_hat = PoeHat()
-    poe_hat.fan_on()
+    # poe_hat.fan_on()
+
+    # font = load_font(size=5)
+    font_2x = load_font(size=10)
     with poe_hat.draw() as draw:
+        draw: ImageDraw
         draw.rectangle(poe_hat.display.bounding_box, outline="white", fill="black")
-        draw.text((10, 10), "Hello World", fill="white")
-    time.sleep(1)
+        draw.text((10, 10), "Hello World", fill="white", font=font_2x)
+
+    time.sleep(10)
     poe_hat.fan_off()
 
 
 def get_device() -> ssd1306:
     serial_display = i2c(port=1, address=0x3C)
     return ssd1306(serial_interface=serial_display, width=128, height=32)
+
+
+def load_font(path='res/cg-pixel-4x5.otf', size=5, **kwargs) -> FreeTypeFont:
+    font_bin = pkgutil.get_data('raspi_poe_mon', path)
+    return ImageFont.truetype(BytesIO(font_bin), size=size, **kwargs)
 
 
 if __name__ == "__main__":
