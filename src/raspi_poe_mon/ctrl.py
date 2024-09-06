42f660ab4aba0d5a5944e938392bafa08115495a
Sebastian Straub <sstraub@posteo.de>
2024-09-06 13:01:08 +0200

    handle signals gracefully

--- src/raspi_poe_mon/ctrl.py
+++ src/raspi_poe_mon/ctrl.py
@@ -11,16 +11,14 @@ logger = logging.getLogger('raspi_poe_mon')
 
 class Controller:
 
-    def __init__(self, fan_on_temp=50.0, fan_off_temp=45.0, frame_time=1.0):
+    def __init__(self, fan_on_temp=50.0, fan_off_temp=45.0, frame_time=1.0, dry_run=False):
         self.fan_on_temp = fan_on_temp
         self.fan_off_temp = fan_off_temp
         self.frame_time = frame_time
-        self.poe_hat = PoeHat()
+        self.poe_hat = PoeHat(dry_run=dry_run)
         self.display = IpDisplay(self.poe_hat)
         self._terminate = False
-        # terminate gracefully on SIGTERM or SIGHUP
-        signal.signal(signal.SIGTERM, self.terminate)
-        signal.signal(signal.SIGHUP, self.terminate)
+        self.add_signal_handlers()
 
     def main_loop(self):
         try:
@@ -48,6 +46,14 @@ class Controller:
         elif self.poe_hat.is_fan_on() and temp < self.fan_off_temp:
             logger.info(f"CPU temperature at {temp}, turning fan OFF")
             self.poe_hat.fan_off()
+
+    def add_signal_handlers(self):
+        try:
+            # terminate gracefully on SIGTERM or SIGHUP
+            signal.signal(signal.SIGTERM, self.terminate)
+            signal.signal(signal.SIGHUP, self.terminate)
+        except AttributeError as e:
+            logger.warning(f"failed to register signal handlers: {e}")
 
     def terminate(self, *args):
         self._terminate = True
